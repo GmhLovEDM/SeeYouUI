c2548fe86a583c1de331e82e83dec1a409e14ae0
GmhLovEDM <gmhlovedm@gmail.com>
2025-11-27 18:10:15 +0800

    优化代码结构

--- components/SeeButton/type.ts
+++ components/SeeButton/type.ts
@@ -1,25 +1,25 @@
 export type TouchEvent = {
-  touches: Array<{ clientX: number; clientY: number }>
-  changedTouches: Array<{ clientX: number; clientY: number }>
-  detail?: {
-    clientX?: number
-    clientY?: number
-  }
-}
+	touches: Array<{ clientX: number; clientY: number }>;
+	changedTouches: Array<{ clientX: number; clientY: number }>;
+	detail?: {
+		clientX?: number;
+		clientY?: number;
+	};
+};
 
 export type ClientRectData = {
-  id?: string
-  dataset?: Record<string, string>
+	id?: string;
+	dataset?: Record<string, string>;
 
-  top: number
-  bottom: number
-  left: number
-  right: number
-  width: number
-  height: number
+	top: number;
+	bottom: number;
+	left: number;
+	right: number;
+	width: number;
+	height: number;
 
-  finalWidth?: number
-}
+	finalWidth?: number;
+};
 
 export interface RippleItem {
 	id: number;
--- pages/index/index.vue
+++ pages/index/index.vue
@@ -8,46 +8,47 @@
 </template>
 
 <script lang="ts" setup>
-	import { ref } from 'vue'
-	
-	type ItemType = {
-		title: string;
-		url: string;
+import { ref } from 'vue';
+
+type ItemType = {
+	title: string;
+	url: string;
+};
+
+const list = ref<ItemType[]>([
+	{
+		title: '按钮',
+		url: '/pages/seeButton/index'
 	}
-	
-	const list = ref<ItemType[]>([
-		{
-			title: '按钮',
-			url: '/pages/seeButton/index'
-		}
-	])
-	
-	const clickItem = (item: ItemType) => uni.navigateTo({
+]);
+
+const clickItem = (item: ItemType) =>
+	uni.navigateTo({
 		url: item.url
-	})
+	});
 </script>
 
 <style>
-	.content {
-		display: flex;
-		flex-direction: column;
-		align-items: center;
-		justify-content: center;
-	}
+.content {
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+}
 
-	.logo {
-		height: 200rpx;
-		width: 200rpx;
-		margin-top: 50rpx;
-		margin-left: auto;
-		margin-right: auto;
-		margin-bottom: 50rpx;
-	}
-	
-	.list{
-		width: 100%;
-		padding: 24rpx 48rpx;
-		box-sizing: border-box;
-		border: 1px #f1f1f1 solid;
-	}
+.logo {
+	height: 200rpx;
+	width: 200rpx;
+	margin-top: 50rpx;
+	margin-left: auto;
+	margin-right: auto;
+	margin-bottom: 50rpx;
+}
+
+.list {
+	width: 100%;
+	padding: 24rpx 48rpx;
+	box-sizing: border-box;
+	border: 1px #f1f1f1 solid;
+}
 </style>
